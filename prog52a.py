65c4a1e4222341058d7f7bfd49fab0989312c406
CightI <111036559+CightI@users.noreply.github.com>
2022-09-09 19:20:05 +0000

    update

---
+++ prog52a.py
@@ -0,0 +1,11 @@
+length = 143
+width = 82
+
+area = length * width
+Perim = 2 * length + 2 * width
+
+print("The legth is"), length
+print("The width is"), width
+
+print("\nThe area is"), area
+print("The perimeter is"), Perim
