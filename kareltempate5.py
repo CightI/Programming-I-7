ad4277166df0b143e85711d67f9f30933d01c317
CightI <111036559+CightI@users.noreply.github.com>
2022-09-27 19:34:57 +0000

    update also 515 is done stil working on 410

---
+++ kareltempate5.py
@@ -0,0 +1,313 @@
+from stanfordkarel import *
+
+
+class ktools:
+
+    def m(self):
+        """Short for Move"""
+        move()
+
+    def tl(self):
+        """Turn Left"""
+        turn_left()
+
+    def tr(self):
+        """Turn Right"""
+        self.tl()
+        self.tl()
+        self.tl()
+
+    def ta(self):
+        """Turn Around"""
+        self.tl()
+        self.tl()
+
+    def pick(self):
+        """Pick Beeper"""
+        pick_beeper()
+
+    def put(self):
+        """Put beeper"""
+        put_beeper()
+
+    def put2(self):
+        """Put 2 beepers in a line"""
+        self.put()
+        self.m()
+        self.put()
+
+    def put5(self):
+        self.put2()
+        self.m()
+        self.put2()
+        self.m()
+        self.put()
+
+    def h(self):
+        """Print H using beepers"""
+        self.tl()
+        self.put5()
+        self.tr()
+        self.m()
+        self.m()
+        self.m()
+        self.tr()
+        self.put5()
+        self.ta()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.put2()
+        self.tl()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.m()
+        self.m()
+        self.m()
+
+    def e(self):
+        """Print E using beepers"""
+
+        self.tl()
+        self.put5()
+        self.tr()
+        self.m()
+        self.put2()
+        self.put2()
+        self.ta()
+        self.m()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.put2()
+        self.put2()
+        self.ta()
+        self.m()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.put2()
+        self.put2()
+        self.m()
+        self.m()
+
+    def l(self):
+        """Print L using beepers"""
+        self.tl()
+        self.put5()
+        self.ta()
+        self.m()
+        self.m()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.put2()
+        self.put2()
+        self.m()
+        self.m()
+
+    def o(self):
+        """Print o using beepers"""
+        self.put2()
+        self.m()
+        self.put()
+        self.tl()
+        self.m()
+        self.put2()
+        self.m()
+        self.put2()
+        self.tl()
+        self.m()
+        self.put2()
+        self.tl()
+        self.m()
+        self.put2()
+        self.m()
+        self.put()
+        self.m()
+        self.tl()
+        self.m()
+        self.m()
+        self.m()
+        self.m()
+
+    def u(self):
+        """Print U using beepers"""
+        self.put2()
+        self.m()
+        self.put()
+        self.tl()
+        self.m()
+        self.put2()
+        self.m()
+        self.put2()
+        self.tl()
+        self.m()
+        self.m()
+        self.put()
+        self.tl()
+        self.m()
+        self.put2()
+        self.m()
+        self.put()
+        self.m()
+        self.tl()
+        self.m()
+        self.m()
+        self.m()
+        self.m()
+
+    def k(self):
+        """Print K using beepers"""
+        self.tl()
+        self.put5()
+        self.ta()
+        self.m()
+        self.m()
+        self.tl()
+        self.m()
+        self.put()
+        self.m()
+        self.tl()
+        self.m()
+        self.put()
+        self.m()
+        self.tr()
+        self.m()
+        self.put()
+        self.tr()
+        self.m()
+        self.m()
+        self.m()
+        self.m()
+        self.tr()
+        self.put()
+        self.m()
+        self.tr()
+        self.m()
+        self.put()
+        self.tr()
+        self.m()
+        self.m()
+        self.m()
+        self.tr()
+        self.m()
+        self.tl()
+
+    def fic(self) -> bool:
+      """Front is Clear"""
+      return front_is_clear()
+
+    def fib(self) -> bool:
+      """Front is Blocked"""
+      return not self.fic()
+
+    def ric(self) -> bool:
+      """Right is clear"""
+      self.tr()
+      if self.fic():
+        self.tl()
+        return True
+        self.tl()
+        return False
+
+    def rib(self) -> bool:
+      """Right is Blocked"""
+      return not self.ric()
+
+
+    def mazemove(self):
+      """Maze Move"""
+      if self.fib():
+        self.tl()
+      else:
+        self.m()
+        if self.ric():
+          self.tr()
+          self.m()
+          if self.ric():
+            self.tr()
+            self.m()
+      pass
+
+    def mm(self, num):
+      """Move Multiple"""
+      for number in range(0, num):
+        self.m()
+
+
+    def putm(self, num):
+        """Put Multiple"""
+        for i in range(num - 1):
+          self.put()
+          self.m()
+          self.put()
+
+    def pickm(self, num):
+      """Pick Multiple"""
+      for _ in range(num - 1):
+        self.pick()
+        self.m()
+        self.pick()
+
+    def SOB(self) -> bool:
+      """standing on beeper"""
+      return beepers_present()
+
+    def jump(self):
+      """jump for 510"""
+      while self.fic():
+        self.m()
+      self.tl()
+      while self.rib():
+        self.m()
+      self.tr()
+      self.m()
+      self.tr()
+      while self.fic():
+        self.m()
+      self.tl()
+
+
+    def find(self):
+      """find for 515"""
+      while not facing_north():
+        self.tl()
+      self.m()
+      if not self.SOB():
+        self.tl()
+        self.m()
+        self.tl()
+        self.m()
+      for _ in range(2):
+        if not self.SOB():
+          self.m()
+          self.tl()
+          self.m()
+      pass
+  
+def main():
+    """ Karel code goes here! """
+    kt = ktools()
+    kt.m()
+    while kt.SOB():
+      kt.pick()
+      kt.find()
+
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
